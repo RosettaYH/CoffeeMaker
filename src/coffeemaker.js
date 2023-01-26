de59953a6bf4160a629f2761872f3fa3ed8818fb
Yoboii.Fuentes - Jose Fuentes <josexfuentes27@gmail.com>
2023-01-26 15:06:20 -0800

    edited test file and created src folder

---
+++ src/coffeemaker.js
@@ -0,0 +1,9 @@
+console.log("Hello world");
+
+export function add(x, y) {
+    return x + y;
+}
+
+export function subtract(x, y) {
+    return x - y;
+}
--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,1 +1,12 @@
-assert 1===1
+import assert from "assert";
+import { add, subtract } from "../src/coffeemaker.js";
+
+describe("The compiler", function () {
+    it("gives the correct values for the add function", function () {
+        assert.equal(add(5, 8), 13);
+        assert.equal(add(5, -8), -3);
+    });
+    it("gives the correct values for the subtract function", function () {
+        assert.equal(subtract(5, 8), -3);
+    });
+});
